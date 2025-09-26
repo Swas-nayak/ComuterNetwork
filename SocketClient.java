359279d7bcfe44a7758ed0453e220923bf6ee79d
Swasthi s nayak <115960554+Swas-nayak@users.noreply.github.com>
2025-09-26 16:01:30 +0530

    Create SocketClient.java

---
+++ SocketClient.java
@@ -0,0 +1,28 @@
+package com.smvitm.cnlab.tcp.ip;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+public class SocketClient {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		Socket clientSocket = new Socket("127.0.0.1",4000);
+		System.out.println("**** Client Side ****");
+		System.out.println("Enter the filename to transfer");
+		String fname = in.nextLine();
+		OutputStream ostream =clientSocket.getOutputStream();
+		PrintWriter pwrite= new PrintWriter(ostream,true);
+		pwrite.println(fname);
+		InputStream istream = clientSocket.getInputStream();
+		BufferedReader socketRead= new BufferedReader( new InputStreamReader(istream));
+		System.out.println("Contents of the file"+ fname+"are");
+		String str;
+		while((str= socketRead.readLine())!= null) {
+			System.out.println(str);
+		}
+		pwrite.close();
+		socketRead.close();
+	}
+
+}
